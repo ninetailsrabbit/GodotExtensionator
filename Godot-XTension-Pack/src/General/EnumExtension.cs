e4b5495338bd52ccd2ebecf5f2ef2fca6093122a
ninetailsrabbit <danieldigitalspace@proton.me>
2024-05-30 10:49:33 +0100

    basic setup to create a class library and publish as a nuget package

---
+++ Godot-XTension-Pack/src/General/EnumExtension.cs
@@ -0,0 +1,38 @@
+﻿namespace Godot_XTension_Pack.src.General {
+    public static class EnumExtension {
+
+        /// <summary>
+        /// Gets a random value from the specified enum type. 
+        /// Example: EnumExtension.RandomEnum<Input.MouseModeEnum>()
+        /// </summary>
+        /// <typeparam name="T">The enum type to get a random value from.</typeparam>
+        /// <param name="enumType">The enum type.</param>
+        /// <returns>A random enum value of type T.</returns>
+        public static T RandomEnum<T>() {
+            T[] values = (T[])Enum.GetValues(typeof(T));
+
+            int randomIndex = new Random().Next(values.Length);
+
+            return values[randomIndex];
+        }
+
+        /// <summary>
+        /// Gets a random value from an enum type.
+        /// Example: Input.MouseModeEnum.Visible.Random()
+        /// </summary>
+        /// <typeparam name="T">The enum type to get a random value from.</typeparam>
+        /// <param name="self">A placeholder parameter for the extension method (ignored).</param>
+        /// <returns>A random enum value of type T.</returns>
+        /// <exception cref="ArgumentException">Throws an ArgumentException if T is not a struct or an Enum.</typeparam>
+        public static T RandomEnum<T>(this T _) where T : struct, Enum {
+            T[] values = Enum.GetValues<T>();
+
+            int randomIndex = new Random().Next(values.Length);
+
+            return values[randomIndex];
+        }
+
+
+    }
+}
+
---
+++ Godot-XTension-Pack/src/General/GenericExtension.cs
@@ -0,0 +1,33 @@
+﻿namespace Godot_XTension_Pack.src.General {
+    public static class GenericExtension {
+        /// <summary>
+        /// Checks if a value is present within a given set of values.
+        /// </summary>
+        /// <typeparam name="T">The type of the value and the elements in the values collection.</typeparam>
+        /// <param name="val">The value to check for.</param>
+        /// <param name="values">A collection of possible values.</param>
+        /// <returns>True if the value is found in the collection; otherwise, false.</returns>
+        /// <remarks>
+        /// This overload uses the `Contains` method of an array to efficiently check for the presence of the value.
+        /// It's limited to value types (`struct`) due to array constraints.
+        /// </remarks>
+        public static bool In<T>(this T val, params T[] values) where T : struct {
+            return values.Contains(val);
+        }
+
+        /// <summary>
+        /// Checks if a value is present within a collection of values.
+        /// </summary>
+        /// <typeparam name="T">The type of the value and the elements in the values collection.</typeparam>
+        /// <param name="val">The value to check for.</param>
+        /// <param name="values">A collection of possible values.</param>
+        /// <returns>True if the value is found in the collection; otherwise, false.</returns>
+        /// <remarks>
+        /// This overload uses the `Contains` method of an `IEnumerable<T>` to allow for various collection types.
+        /// It works with both value types (`struct`) and reference types (`class`).
+        /// </remarks>
+        public static bool In<T>(this T val, IEnumerable<T> values) where T : struct {
+            return values.Contains(val);
+        }
+    }
+}
