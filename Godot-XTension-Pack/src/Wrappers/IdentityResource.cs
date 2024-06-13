da100937f862f08bb2834aa383509b2be9b47411
ninetailsrabbit <danieldigitalspace@proton.me>
2024-06-13 09:25:50 +0100

    fixed nextFloat and added two new classes: Singleton and IdentityResource

---
+++ Godot-XTension-Pack/src/Wrappers/IdentityResource.cs
@@ -0,0 +1,25 @@
+﻿using Godot;
+using Godot_XTension_Pack;
+
+namespace Godot_XTension_Packs {
+
+    [Tool]
+    [GlobalClass]
+    public abstract partial class IdentityResource : Resource {
+
+        public string Id { get; private set; }
+
+        private Guid? _guid;
+
+        public Guid GetId() {
+            _guid ??= Guid.Parse(Id);
+
+            return _guid.Value;
+        }
+
+        public IdentityResource() {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+
+}
