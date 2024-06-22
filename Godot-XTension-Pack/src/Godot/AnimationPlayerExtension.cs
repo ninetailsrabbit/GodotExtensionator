d17e4c447f141da65e1052186ae43efad186c6eb
ninetailsrabbit <danieldigitalspace@proton.me>
2024-06-22 09:39:49 +0100

    some new methods to extend behaviour on animation player and transform

---
+++ Godot-XTension-Pack/src/Godot/AnimationPlayerExtension.cs
@@ -0,0 +1,39 @@
+﻿using Godot;
+
+namespace Godot_XTension_Pack {
+    public static class AnimationPlayerExtension {
+
+        const string RESET_ANIMATION = "RESET";
+
+        public static void Queue(this AnimationPlayer animationPlayer, string name, Action finishedCallback, float? delay = null) {
+            animationPlayer.Queue(name, finishedCallback, delay);
+        }
+        public static void Queue(this AnimationPlayer animationPlayer, StringName name, Action finishedCallback, float? delay = null) {
+            var animation = animationPlayer.GetAnimation(name);
+            delay ??= animation.Length;
+
+            ArgumentNullException.ThrowIfNull(animation);
+
+            var timer = animationPlayer.GetTree().CreateTimer((double)delay);
+            timer.Connect(SceneTreeTimer.SignalName.Timeout, Callable.From(finishedCallback));
+
+            animationPlayer.Queue(name);
+        }
+
+        public static void QueueAndReset(this AnimationPlayer animationPlayer, string name, Action finishedCallback, float? delay = null) {
+            animationPlayer.QueueAndReset(new StringName(name), finishedCallback, delay);
+        }
+
+        public static void QueueAndReset(this AnimationPlayer animationPlayer, StringName name, Action finishedCallback, float? delay = null) {
+            animationPlayer.Queue(name, finishedCallback);
+            animationPlayer.Reset();
+        }
+
+
+        public static void Reset(this AnimationPlayer animationPlayer) {
+            if (animationPlayer.HasAnimation(RESET_ANIMATION))
+                animationPlayer.Queue(RESET_ANIMATION);
+        }
+
+    }
+}
---
+++ Godot-XTension-Pack/src/Godot/Transform2DExtension.cs
@@ -0,0 +1,34 @@
+﻿using Godot;
+
+namespace Godot_XTension_Pack {
+    public static class Transform2DExtension {
+
+        /// <summary>
+        /// Gets the right unit vector of the transform's local space.
+        /// </summary>
+        /// <param name="transform">The Transform2D instance.</param>
+        /// <returns>A Vector2 representing the right unit vector (1, 0) in the transform's local space.</returns>
+        public static Vector2 Right(this Transform2D transform) => transform.X;
+
+        /// <summary>
+        /// Gets the left unit vector of the transform's local space.
+        /// </summary>
+        /// <param name="transform">The Transform2D instance.</param>
+        /// <returns>A Vector2 representing the left unit vector (-1, 0) in the transform's local space.</returns>
+        public static Vector2 Left(this Transform2D transform) => -transform.X;
+
+        /// <summary>
+        /// Gets the up unit vector of the transform's local space.
+        /// </summary>
+        /// <param name="transform">The Transform2D instance.</param>
+        /// <returns>A Vector2 representing the up unit vector (0, -1) in the transform's local space.</returns>
+        public static Vector2 Up(this Transform2D transform) => -transform.Y;
+
+        /// <summary>
+        /// Gets the down unit vector of the transform's local space.
+        /// </summary>
+        /// <param name="transform">The Transform2D instance.</param>
+        /// <returns>A Vector2 representing the down unit vector (0, 1) in the transform's local space.</returns>
+        public static Vector2 Down(this Transform2D transform) => transform.Y;
+    }
+}
