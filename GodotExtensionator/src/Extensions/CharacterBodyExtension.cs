99506d46bb551739bd4e337934df866b675333df
ninetailsrabbit <danieldigitalspace@proton.me>
2024-07-27 16:36:47 +0100

    function to retrieve current speed in kmh from a character body 3d

---
+++ GodotExtensionator/src/Extensions/CharacterBodyExtension.cs
@@ -0,0 +1,9 @@
+﻿using Godot;
+
+namespace GodotExtensionator {
+    public static class CharacterBodyExtension {
+
+        public static float CurrentSpeed(this CharacterBody3D body)
+            => Mathf.Max(new Vector3(body.Velocity.X, 0f, body.Velocity.Z).Length(), Mathf.Clamp(Mathf.Abs(body.Velocity.Y), 0f, float.MaxValue));
+    }
+}
